56ed3270ff42d95760a842f2452167c00fc1a274
k_oonaka <k_oonaka@n-c-c.co.jp>
2022-01-22 23:29:15 +0900

    pwa化

---
+++ docs/service-worker.js
@@ -0,0 +1,45 @@
+// キャッシュファイルの指定
+var CACHE_NAME = "pwa-mobile-caches";
+var urlsToCache = [
+    "/offline.htm",
+];
+
+// インストール処理
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                if(response){
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+//メッセージを受け取ったとき
+self.addEventListener('message', function(event) {
+    switch (event.data) {
+        case 'updateCache':
+            event.waitUntil(
+                caches
+                .open(CACHE_NAME)
+                .then(function(cache){
+                    return cache.addAll(urlsToCache);
+                })
+            );
+            break;
+        default:
+            break;
+    }
+});
