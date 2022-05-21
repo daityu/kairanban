77e6a562c0eee09b8a0b4f64d5f9a8c36ca4b9b2
k_oonaka <k_oonaka@n-c-c.co.jp>
2022-05-21 11:59:37 +0900

    パスコード暗号化

--- docs/libs/Common.js
+++ docs/libs/Common.js
@@ -5,16 +5,15 @@
  *  @param objext obj [description]
  */
 function is(type, obj) {
-    var clas  = Object.prototype.toString.call(obj).slice(8, -1);
-    return obj !== undefined && obj !== null && clas === type;
+  var clas = Object.prototype.toString.call(obj).slice(8, -1);
+  return obj !== undefined && obj !== null && clas === type;
 }
 /**
  * フォーマット関数
  *  "あいう{0},かきく{1}".format(["えお","けこ"]); //->あいうえお,かきくけこ
  */
 if (String.prototype.format == undefined) {
-  String.prototype.format = function(arg)
-  {
+  String.prototype.format = function (arg) {
     // 置換ファンク
     var rep_fn = undefined;
 
@@ -23,24 +22,23 @@ if (String.prototype.format == undefined) {
     }
     // オブジェクトの場合
     if (typeof arg == "object") {
-      rep_fn = function(m, k) { return arg[k]; }
+      rep_fn = function (m, k) { return arg[k]; }
     }
     // 複数引数だった場合
     else {
       var args = arguments;
-      rep_fn = function(m, k) { return args[ parseInt(k) ]; }
+      rep_fn = function (m, k) { return args[parseInt(k)]; }
     }
 
-    return this.replace( /\{(\w+)\}/g, rep_fn );
+    return this.replace(/\{(\w+)\}/g, rep_fn);
   }
 }
 /**
  * 連想配列連結関数
  *  [{name:"あああ"},{name:"いいい"}].renketsu("name"); //->あああ\n\nいいい
  */
- if (Array.prototype.renketsu == undefined) {
-  Array.prototype.renketsu = function(arg)
-  {
+if (Array.prototype.renketsu == undefined) {
+  Array.prototype.renketsu = function (arg) {
     // 置換ファンク
     var rep_fn = undefined;
 
